32fd6f0c25d9402d32de1b544dd0753b4578afa6
ana fabia <ana fabia@DESKTOP-R2DJUD1>
2017-11-09 15:29:30 -0200

    alterado arquivo para aula 09/11

---
+++ src/Peojeto/Gastos.java
@@ -0,0 +1,12 @@
+
+package Peojeto;
+
+
+public class Gastos {
+    public static void main (String args[]){
+          Tela1 t1 = new Tela1();
+          t1.setVisible(true);
+          t1.setLocationRelativeTo(null);
+            
+    }        
+}
